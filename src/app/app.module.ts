2ab54143b787d28d95cb46282edb1238d00f8cb8
Ivan Leuskov <ileuskov@gmail.com>
2022-09-03 16:16:17 +0200

    try to use bootstrap :lipstick:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { EventsAppComponent } from './events-app.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { EventListComponent } from './events/events-list.components';
+import { NavBarComponent } from './nav/navbar.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { EventListComponent } from './events/events-list.components';
     EventsAppComponent,
     EventListComponent,
     EventThumbnailComponent,
+    NavBarComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
