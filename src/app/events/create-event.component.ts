c480db8d5df0deef93e596b7e88b4a0c3c9cf752
Ivan Leuskov <ileuskov@gmail.com>
2022-09-09 15:34:07 +0200

    make create event component using template driven forms

--- src/app/events/create-event.component.ts
+++ src/app/events/create-event.component.ts
@@ -1,12 +1,45 @@
+import { eventService } from './shared/event.service';
+import { IEvent } from './shared/event.model';
 import { Router } from '@angular/router';
 import { Component } from '@angular/core';
 
 @Component({
-  templateUrl: './create-event.template.html',
+  templateUrl: './create-event.component.html',
+  styles: [
+    `
+      em {
+        float: right;
+        color: #e05c65;
+        padding-left: 10px;
+      }
+      .error input {
+        background-color: #e3c3c5;
+      }
+      .error ::-webkit-input-placeholder {
+        color: #999;
+      }
+      .error ::-moz-placeholder {
+        color: #999;
+      }
+      .error :-moz-placeholder {
+        color: #999;
+      }
+      .error ::-ms-input-placeholder {
+        color: #999;
+      }
+    `,
+  ],
 })
 export class CreateEventComponent {
+  newEvent!: any;
   isDirty: boolean = true;
-  constructor(private router: Router) {}
+  constructor(private router: Router, private eventService: eventService) {}
+
+  saveEvent(newEventFormValues: any) {
+    this.eventService.saveEvent(newEventFormValues);
+    this.isDirty = false;
+    this.router.navigate(['/events']);
+  }
   cancel() {
     this.router.navigate(['/events']);
   }
