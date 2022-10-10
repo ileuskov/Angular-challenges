1fd27b7b41e1c4257a01ec486220f0d5718cd4f4
Ivan Leuskov <ileuskov@gmail.com>
2022-10-10 17:21:13 +0200

    refactor auth service to use http service and manage data on the server

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -1,16 +1,44 @@
 import { IUser } from './user.model';
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, tap } from 'rxjs/operators';
+import { of, Observable } from 'rxjs';
 
 @Injectable()
 export class AuthService {
-  currentUser!: IUser;
-  loginUser(userName: string, password: string) {
-    this.currentUser = {
-      id: 1,
-      userName: userName,
-      firstName: 'Rick',
-      lastName: 'Sanchez',
+  constructor(private http: HttpClient) {}
+  currentUser!: any;
+  loginUser(userName: string, password: string): Observable<any> {
+    let loginInfo = {
+      username: userName,
+      password: password,
     };
+    let options = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+    };
+
+    return this.http
+      .post('/api/login', loginInfo, options)
+      .pipe(
+        tap((data: any) => {
+          this.currentUser = <IUser>data['user'];
+        })
+      )
+
+      .pipe(
+        catchError((err) => {
+          return of(false);
+        })
+      );
+
+    // this.currentUser = {
+    //   id: 1,
+    //   userName: userName,
+    //   firstName: 'Rick',
+    //   lastName: 'Sanchez',
+    // };
   }
 
   isAuthenticated() {
@@ -18,8 +46,47 @@ export class AuthService {
     return !!this.currentUser;
   }
 
-  updateCurrentUser(firstName: string, lastName: string) {
+  checkAuthenticationStatus() {
+    //by not returning it as an observable and directly subscribing to this observable,
+    // we can just call the method in events-app without subscribing
+    this.http
+      .get('/api/currentIdentity')
+      .pipe(
+        tap((data) => {
+          if (data instanceof Object) {
+            this.currentUser = <IUser>data;
+          }
+        })
+      )
+      .subscribe();
+  }
+
+  updateCurrentUser(firstName: string, lastName: string): Observable<any> {
+    //the api server is set up to handle such calls in form of /api/users/id
     this.currentUser.firstName = firstName;
     this.currentUser.lastName = lastName;
+
+    let options = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+    };
+
+    return this.http.put(
+      `/api/users/${this.currentUser.id}`,
+      this.currentUser,
+      options
+    );
+  }
+
+  logout() {
+    this.currentUser = undefined;
+    let options = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+    };
+
+    return this.http.post('/api/logout', {}, options);
   }
 }
